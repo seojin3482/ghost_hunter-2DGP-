c865d6516629997fa605398dcbad2305527de28c
seojin3482 <seojin3482@naver.com>
2022-11-17 07:27:31 +0900

    ghost 속도 수정

--- ghost_hunter/ghost.py
+++ ghost_hunter/ghost.py
@@ -1,5 +1,21 @@
 from pico2d import *
 import random
+import game_framework
+
+
+# Ghost Run Speed
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 5.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
+# Ghost action Speed (frame)
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION_RUN = 4
+
 
 
 class Ghost:
@@ -12,9 +28,8 @@ class Ghost:
 
 
     def update(self):
-        self.frame = (self.frame + 1) % 4+4
-        self.frame2 = (self.frame2 + 1) % 4
-        self.x += self.dir * 1
+        self.frame = (self.frame + FRAMES_PER_ACTION_RUN * ACTION_PER_TIME * game_framework.frame_time) % 4+4
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time #방향*속도*시간
         if self.x > 800:
             self.x = 800
             self.dir = -1 #왼쪽
@@ -25,13 +40,13 @@ class Ghost:
 
     def draw(self):
         if self.dir == 1:
-            self.image.clip_draw(self.frame*144, 128, 144, 128,self.x, self.y)
-            delay(0.01)
+            self.image.clip_draw(int(self.frame)*144, 128, 144, 128,self.x, self.y)
+
 
         else:
+            self.image.clip_composite_draw(int(self.frame)*144, 128, 144, 128, -3.141592, 'v', self.x, self.y, 144, 128)
 
-            self.image.clip_draw(self.frame2*144, 128, 144, 128, self.x, self.y)
-            delay(0.01)
+
 
 
 
