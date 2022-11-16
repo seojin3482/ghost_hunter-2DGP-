e0d5a12933de9cf21eec05993ece76dc8d2503bb
seojin3482 <seojin3482@naver.com>
2022-11-17 07:06:49 +0900

    hunter 시간
    hunter 위치/프레임 변화

--- ghost_hunter/hunter.py
+++ ghost_hunter/hunter.py
@@ -3,16 +3,17 @@ import game_framework
 
 # Boy Run Speed
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0 # Km / Hour
+RUN_SPEED_KMPH = 25.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 
-# Boy Action Speed
-TIME_PER_ACTION = 0.5
+# Hunter run Speed
+TIME_PER_ACTION = 0.1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+FRAMES_PER_ACTION_RUN = 3
+FRAMES_PER_ACTION_SLEEP=3
 
 
 
@@ -79,16 +80,16 @@ class RUN:
 
 
     def do(self):
-        self.frame = (self.frame + 1) % 3 + 6
-        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
+        self.frame = (self.frame + FRAMES_PER_ACTION_RUN * ACTION_PER_TIME * game_framework.frame_time) % 3+6
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time #방향*속도*시간
         self.x = clamp(0, self.x, 800)
 
     def draw(self):
         if self.dir == 1:
-            self.image.clip_draw(self.frame * 113, 569, 113, 113, self.x, self.y)
+            self.image.clip_draw(int(self.frame) * 113, 569, 113, 113, self.x, self.y)
             delay(0.01)
         elif self.dir == -1:
-            self.image.clip_composite_draw(self.frame * 113, 569, 113, 133, -3.141592, 'v', self.x, self.y, 113, 113)
+            self.image.clip_composite_draw(int(self.frame) * 113, 569, 113, 133, -3.141592, 'v', self.x, self.y, 113, 113)
             delay(0.01)
 
 
@@ -102,14 +103,14 @@ class SLEEP:
         pass
 
     def do(self):
-        self.frame = (self.frame + 1) % 3+6 #6,7,8
+        self.frame = (self.frame + FRAMES_PER_ACTION_SLEEP * ACTION_PER_TIME * game_framework.frame_time) % 3+6 #6,7,8
 
     def draw(self):
         if self.face_dir == -1:
-            self.image.clip_composite_draw(self.frame * 113, 113, 113, 113,
+            self.image.clip_composite_draw(int(self.frame) * 113, 113, 113, 113,
                                           0, '', self.x + 25, self.y+10 , 113, 113)
         else:
-            self.image.clip_composite_draw(self.frame * 113, 113, 113, 113,
+            self.image.clip_composite_draw(int(self.frame) * 113, 113, 113, 113,
                                           -3.141592, 'v', self.x - 25, self.y+10, 113, 113)
 
 
