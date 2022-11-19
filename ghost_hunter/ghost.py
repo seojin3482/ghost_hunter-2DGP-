8909c6f962def38b919a1dac0abe11c09ab1a032
seojin3482 <seojin3482@naver.com>
2022-11-19 23:01:21 +0900

    effect/ghost 충돌 체크

--- ghost_hunter/ghost.py
+++ ghost_hunter/ghost.py
@@ -22,9 +22,11 @@ class Ghost:
     def __init__(self):
         self.x, self.y = random.randint(0,800), 160
         self.frame = 0
-        self.frame2 = 0
         self.dir = 1 # 오른쪽
         self.image = load_image('ghost.png')
+        self.hp=100
+        self.font = load_font('ENCR10B.TTF', 16)
+
 
 
     def update(self):
@@ -43,12 +45,30 @@ class Ghost:
             self.image.clip_draw(int(self.frame)*144, 128, 144, 128,self.x, self.y)
         else:
             self.image.clip_composite_draw(int(self.frame)*144, 128, 144, 128, -3.141592, 'v', self.x, self.y, 144, 128)
-        draw_rectangle(*self.get_bb())
+        #draw_rectangle(*self.get_bb())
+
+        #self.font.draw(100, 500, f'(hp:{self.hp:.2f})', (255, 0, 0))
 
     def get_bb(self):
-        return self.x - 40, self.y - 40, self.x + 40, self.y + 40
+        return self.x - 20, self.y - 40, self.x + 20, self.y + 40
 
     def handle_collision(self, other, group):
-        pass
+        if group=='effect:team':
+            self.hp-=1
+            if self.dir > 0:
+                self.image.clip_draw(144*7, 128*6, 144, 128, self.x, self.y)
+            else:
+                self.image.clip_composite_draw(144*7, 128*6, 144, 128, -3.141592, 'v', self.x, self.y,
+                                               144, 128)
+
+
+
+    # def ghost_damage(self):
+    #     self.timer=10
+    #     while self.timer>0:
+
+
+
+
 
 
